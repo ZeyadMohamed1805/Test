7812c064ce54e6b9b3572e706137eacb299fb0b1
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-03-07 23:03:00 +0200

    Updated Zeyad Models

--- Test/Test/Models/Tables/User.cs
+++ Test/Test/Models/Tables/User.cs
@@ -1,6 +1,12 @@
-﻿namespace Test.Models.Tables
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test.Models.Tables
 {
     public class User
     {
+        [Key]
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+        public ICollection<Role> Roles { get; set; }
     }
 }
