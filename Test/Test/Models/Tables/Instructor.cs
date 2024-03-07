66085d404e33d2fd437023a2a6ee227b96f186f9
DESKTOP-OD70FRL\mohamed kadry <muhammadkadry22@gmail.com>
2024-03-07 23:15:32 +0200

    first Commit add instructor

--- Test/Test/Models/Tables/Instructor.cs
+++ Test/Test/Models/Tables/Instructor.cs
@@ -1,6 +1,22 @@
-﻿namespace Test.Models.Tables
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Test.Models.Tables
 {
     public class Instructor
     {
+        public int InstructorID { get; set; }
+        [Required]
+        public String InstructorFirstName { get; set; }
+        public String InstructorLastName { get; set; } ///
+        public int InstructorAge { get; set; }
+        public int InstructorSalary { get; set; }
+        public String InstructorAddress { get; set; }
+
+        [Required(ErrorMessage = "The Email field is required.")]
+        [EmailAddress(ErrorMessage = "The Email field is not a valid e-mail address.")]
+        public String InstructorEmail { get; set; }
+        [ForeignKey("InstructorEmail")]
+        public User User { get; set; }
     }
 }
