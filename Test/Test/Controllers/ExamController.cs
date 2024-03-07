fc7ef54df890a944824942bfcd40a52812be0f44
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-03-07 22:55:44 +0200

    New Project

---
+++ Test/Test/Controllers/ExamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Test.Controllers
+{
+    public class ExamController : Controller
+    {
+        public IActionResult Course(int Id)
+        {
+            return View();
+        }
+    }
+}
---
+++ Test/Test/Models/Tables/Admin.cs
@@ -0,0 +1,6 @@
+﻿namespace Test.Models.Tables
+{
+    public class Admin
+    {
+    }
+}
