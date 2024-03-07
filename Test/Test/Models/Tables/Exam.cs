ac9df300bbde0b4acfecf11dbf175a395cd8046e
DESKTOP-OD70FRL\mohamed kadry <muhammadkadry22@gmail.com>
2024-03-07 23:19:38 +0200

    Add exam and Department

--- Test/Test/Models/Tables/Exam.cs
+++ Test/Test/Models/Tables/Exam.cs
@@ -1,6 +1,18 @@
-﻿namespace Test.Models.Tables
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Test.Models.Tables
 {
     public class Exam
     {
+        public int DepartmentID { get; set; }
+        [Required]
+        public String DepartmentName { get; set; }
+
+
+        //Supervisor?
+        public int DepartmentSP { get; set; }
+        [ForeignKey("DepartmentSP")]
+        public Instructor Instructor { get; set; }
     }
 }
